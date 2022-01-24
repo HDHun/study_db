f195afdeb65a06c76028c9bc309728733d6b099b
HDHun <pooekdma@naver.com>
2022-01-24 11:52:04 +0900

    sql

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,24 @@
+use world;
+
+show tables;
+
+-- 주석
+
+-- 테이블생성
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+    
+);
+
+-- 테이블 삭제
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+    
+);
