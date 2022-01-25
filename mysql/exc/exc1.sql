45f80c606766d94d6e8d60cd58e0b76bf4e30175
HDHun <pooekdma@naver.com>
2022-01-25 17:24:15 +0900

    sql

--- mysql/exc/exc1.sql
+++ mysql/exc/exc1.sql
@@ -1,40 +1,17 @@
 use world;
 
-create table cafe (
-	seq int,
-    cafename varchar(45),
-    tableNumber int(100),
-    chairNumber int(100),
-    address varchar(45),
-    area varchar(45),
-    representativeName varchar(45)
-    );
-
-
-
-
-insert into cafe (
-	seq
-    ,cafename
-    ,tableNumber
-    ,chairNumber
-    ,address
-    ,area
-    ,representativeName)
-    values (
-	1
-    , '별바당'
-    , 1
-    , 2
-    , '서울시 어디읍'
-    , '1'
-    , '장동건'
+insert into music (
+seq
+,intro
+,room
+,time
+,personnel
+,facility)
+value (
+1
+,'레슨,성악,보컬,피아노,악기 연습등 원하는 음악을 마음껏 할 수 있는 지상 연습실(단,금관악기는 제외)'
+,'공간유형 보컬연습실 악기연습실'
+,'최소 1시간부터'
+,4
+,'의자/테이블, 전신거울, 내부화장실, 금연, 공용라운지, 샤워시설, 인터넷/WIFI'
 );
-
--- varchar는 '또는"으로 감싼다
--- int는 안사용해도 된다.
--- 컴마는 앞쪽을 선호 :는 호불호가 갈린다.
-
--- 데이터 조회
-
-select *from cafe;
---
+++ mysql/exc/exc3.sql
@@ -0,0 +1,35 @@
+use world;
+
+
+insert into book (
+	seq
+    ,bookImage
+    ,bookHeader
+    ,bookName
+    ,writer
+    ,bookCompany
+    ,publishingDate
+    ,price 
+    ,quantity
+    ,review
+    ,arrival
+    ,event 
+    ,relatedProducts 
+    ,ebookPrice)
+    values (
+    1
+    ,'image'
+    ,'이지스퍼블리싱-Do it! 시리즈'
+    ,'[도서]Do it! HTML+CSS+자바스크립트 웹 표준의 정석 한권으로 끝내는 웹 기본 교과서'
+    ,'고경희 저'
+    ,'이지스퍼블리싱'
+    ,'2021-01-01 12:00:00'
+    ,27000
+    ,'판매지수 49,899'
+    ,'회원리뷰(15건)'
+    ,'2021-01-25 23:49:49'
+    ,'[대학생X취준생]지치지 않는 자, 점수를 잡는다! - 리유저블텀블러/한국문학 담요/슬라이드 파일 증정'
+    ,'관련상품 : 중고상품 11개'
+    ,19000
+    );
+    
