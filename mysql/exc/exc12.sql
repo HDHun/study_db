352e2f203252ee4c94aec889a2a74f3764664042
HDHun <pooekdma@naver.com>
2022-02-08 17:38:19 +0900

    db

---
+++ mysql/exc/exc12.sql
@@ -0,0 +1,26 @@
+INSERT INTO `ppap`.`infrmemberjoinqna`
+(
+`ifjqQuestionCd`,
+`ifjqAnswer`,
+`ifmaDelNy`,
+`regDateTime`,
+`regDateTimeSvr`,
+`modDateTime`,
+`modDateTimeSvr`,
+`ifmmSeq`)
+VALUES
+(
+<{ifjqQuestionCd: }>,
+<{ifjqAnswer: }>,
+<{ifmaDelNy: }>,
+<{regDateTime: }>,
+<{regDateTimeSvr: }>,
+<{modDateTime: }>,
+<{modDateTimeSvr: }>,
+<{ifmmSeq: }>);
+
+
+
+
+select*from infrcodeGroup;
+select*from infrcode;
