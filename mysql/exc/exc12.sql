009287343431faea317e0dac7377f5b0d370dc27
HDHun <pooekdma@naver.com>
2022-02-14 17:06:01 +0900

    db

--- mysql/exc/exc12.sql
+++ mysql/exc/exc12.sql
@@ -3,8 +3,6 @@
 
 
 
-
-
 select 
 a.ifcgSeq
 ,a.ifcgName
@@ -84,3 +82,13 @@ select*from infrmemberphone;
 select*from infrauthmenu;
 select*from infrcode;
 select*from infrnationality;
+
+select 
+a.ifatSeq
+,(select ifatName from infrAuth where ifatSeq = a.ifatSeq) as ifatName
+,a.ifmuSeq
+,(select ifmuName from infrMenu where ifmuSeq = a.ifmuSeq) as ifmuName
+,a.ifauRoleCd
+,(select ifcdName from infrCode where ifcdSeq = a.ifauRoleCd) as ifauRoleName
+from infrAuthMenu a;
+
